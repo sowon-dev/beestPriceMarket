b8f0325d6a3733fe971fcc14255cf684772d5d21
jekk0215 <vividdream0215@naver.com>
2020-10-26 09:33:54 +0900

    상품 게시판 view 연결

---
+++ src/main/java/com/bestpricemarket/controller/GoodsController.java
@@ -0,0 +1,58 @@
+package com.bestpricemarket.controller;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bestpricemarket.service.GoodsService;
+
+@Controller
+@RequestMapping(value = "/goods/*")
+public class GoodsController {
+	
+	private static final Logger log = LoggerFactory.getLogger(GoodsController.class);
+	
+	// 서비스 의존 주입 
+	@Inject
+	private GoodsService service;
+	
+	@RequestMapping(value = "/register",method = RequestMethod.GET)
+	// 상품등록
+	public String goodsRegisterGET() throws Exception{
+		
+		log.info("goodsRegister.jsp 이동");
+		
+		return "/goods/goodsRegister";
+	}
+	
+	
+	// 상품목록
+	@RequestMapping(value = "/list",method = RequestMethod.GET)
+	public String goodsListGET() throws Exception{
+		
+		log.info("goodsList.jsp 이동");
+		
+		return "/goods/goodsList";
+	}
+	
+	
+	// 상품조회(상품상세페이지)
+	@RequestMapping(value = "/detail",method = RequestMethod.GET)
+	public String goodsDetailGET() throws Exception{
+		log.info("goodsDetail.jsp 이동");
+		
+		return "/goods/goodsDetail";
+	}
+	
+	// 상품수정
+	
+	// 상품삭제
+	
+	
+	
+
+}
---
+++ src/main/java/com/bestpricemarket/persistence/GoodsDAO.java
@@ -0,0 +1,15 @@
+package com.bestpricemarket.persistence;
+
+public interface GoodsDAO {
+	
+	// 상품등록
+	
+	// 상품목록
+		
+	// 상품조회(상품상세페이지)
+		
+	// 상품수정
+		
+	// 상품삭제
+
+}
